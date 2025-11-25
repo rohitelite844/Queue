02a077b8a98e96e0c16d039f4bf4ed3866dd4fa6
Rohit Ingle <rohitingle126@gmail.com>
2025-11-25 18:53:22 +0530

    Add files via upload

---
+++ PQ10.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class PQ10 {
+
+	public static void main(String[] args) {
+		PriorityQueue<String> queue = new PriorityQueue();
+		queue.add("Black");
+		queue.add("White");
+		queue.add("Red");
+		queue.add("Green");
+		queue.add("Blue");
+		
+		System.out.println("Elements of the Priority Queue :");
+		System.out.println(queue);
+		
+	
+	}
+}
